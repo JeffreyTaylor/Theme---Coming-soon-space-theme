5f7a572073ca16d3d0895d75dedc945a7e79dc8c
Jeffrey Taylor <jeff@thetaylors.com>
2017-09-14 22:23:12 -0400

    add gulp stuff for deployment

--- gulpfile.js
+++ gulpfile.js
@@ -6,24 +6,35 @@ const del = require('del');
 
 gulp.task('move-src', function () {
   return gulp.src('./src/**/*')
-  .pipe(gulp.dest('./dist/src'));
+  .pipe(gulp.dest('./dist/files/src'));
 });
 
 gulp.task('move-help', function () {
   return gulp.src('./help/**/*')
-  .pipe(gulp.dest('./dist/help'));
+  .pipe(gulp.dest('./dist/files/help'));
 });
 
-gulp.task('zip', function () {
-  return gulp.src('./dist/**/*')
-  .pipe(zip('archive.zip'))
+gulp.task('move-marketing', function () {
+  return gulp.src('./marketing/**/*')
+  .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('zip-files', function () {
+  return gulp.src('./dist/files/**/*')
+  .pipe(zip('file-archive.zip'))
+  .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('zip-preview-images', function () {
+  return gulp.src('./dist/preview*')
+  .pipe(zip('image-archive.zip'))
   .pipe(gulp.dest('./dist'))
 });
 
 gulp.task('cleanup', function () {
   return del([
-    './dist/src',
-    './dist/help'
+    './dist/files',
+    './dist/preview*'
   ]);
 });
 
@@ -33,7 +44,9 @@ gulp.task('default', function (done) {
   return runSequence(
     'move-src',
     'move-help',
-    'zip',
+    'move-marketing',
+    'zip-files',
+    'zip-preview-images',
     'cleanup',
     done);
 });
